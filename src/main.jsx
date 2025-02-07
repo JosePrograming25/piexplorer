00bb0ac4fda56bb5d29e0263c5980ce70a10155f
JosePrograming25 <joseprograming25@gmail.com>
2025-02-06 20:17:06 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom/client"
+import App from "./App.jsx"
+
+const root = document.getElementById("root")
+
+ReactDom.createRoot(root).render(<App />)
